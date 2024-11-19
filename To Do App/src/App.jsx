e144402ce42ce3c20d670a7134af957fc7c6ec04
Rana Umair <ranaumairfyp@gmail.com>
2024-11-18 20:40:56 -0800

    11

--- To Do App/src/App.jsx
+++ To Do App/src/App.jsx
@@ -1,12 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import AppName from "./Components/AppName";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return 
+  return (
+
+    <center class="todo-container">
+      <AppName />
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter Task Here" />
+          </div>
+          <div class="col-4">
+            <input type="date" placeholder="Enter Task Here" />
+          </div>
+          <div class="col-2">
+            <button class="btn btn-success">Add</button>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-6">
+            Buy Milk
+          </div>
+          <div class="col-4">
+            4/10/2023
+          </div>
+          <div class="col-2">
+            <button class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            Go To College
+          </div>
+          <div class="col-4">
+            4/10/2023
+          </div>
+          <div class="col-2">
+            <button class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+
+      </div>
+    </center>
+  )
 }
 
 export default App
---
+++ To Do App/src/Components/AppName.jsx
@@ -0,0 +1,5 @@
+function AppName(){
+    return <center style={{"font-size": "36px", "text-align": "center"}}>TODO React App</center>
+}
+
+export default AppName;
