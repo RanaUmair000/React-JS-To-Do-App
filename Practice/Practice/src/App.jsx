96b3effd24e2413185e7ec67d46b1e7a11dcb43c
Rana Umair <ranaumairfyp@gmail.com>
2024-11-20 18:23:14 -0800

    f

--- Practice/Practice/src/App.jsx
+++ Practice/Practice/src/App.jsx
@@ -1,17 +1,18 @@
 import Error from "./Components/Error";
 import MainList from "./Components/Main List";
+import Container from "./Components/Container";
 
 function App() {
 
   let foods = ['Milk', 'Mutton', 'Butter', 'Yogurt'];
 
   return (
-    <>
+    <Container>
       <h1>FoodItems List</h1>
 
       <Error items={foods} />
       <MainList items={foods} />
-    </>
+    </Container>
   )
 }
 
---
+++ Practice/Practice/src/Components/Container.jsx
@@ -0,0 +1,5 @@
+let Container = (props) => {
+    return <div class="container">{props.children}</div>
+}
+
+export default Container;
