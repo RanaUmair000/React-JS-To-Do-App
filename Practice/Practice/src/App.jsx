81701ff7e3e44f780cc76d0a951638ab1106af82
Rana Umair <ranaumairfyp@gmail.com>
2024-11-23 00:12:41 -0800

    f

--- Practice/Practice/src/App.jsx
+++ Practice/Practice/src/App.jsx
@@ -1,22 +1,32 @@
 import Error from "./Components/Error";
 import MainList from "./Components/Main List";
 import Container from "./Components/Container";
-
-function inputFunction(event){
-  console.log(event.target.value);
-}
+import './App.css';
+import { useState } from "react";
 
 function App() {
 
-  let foods = ['Milk', 'Mutton', 'Butter', 'Yogurt'];
+
+  let[foods, setItems] = useState([]);
+
+  function keyDownFunc(event){
+    if(event.key == 'Enter'){
+      let inputItem = event.target.value;
+      event.target.value = '';
+      let newItems = [...foods, inputItem];
+      setItems(newItems);
+    }
+    
+  }
 
   return (
     <Container>
       <h1>FoodItems List</h1>
-
-      <input type="text" placeholder="Enter Something Here" onChange={(event) => inputFunction(event)} />
-
-      <Error items={foods} />
+      <div class="input-sec">
+        <input type="text" placeholder="Enter Something Here" onKeyDown={(event) => keyDownFunc(event)} />
+        <button class="btn btn-info">Add</button>
+      </div>
+      <Error items={foods} /> 
       <MainList items={foods} />
     </Container>
   )
